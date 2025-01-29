f370164ce8bccdf3754dc209529d87762525e32e
ahmedMaherE <asebaee@youxel.com>
2025-01-29 11:07:44 +0200

    Project strucure

---
+++ DFComponents/DFComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DFComponentsApp.swift
+//  DFComponents
+//
+//  Created by ahmed maher on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DFComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DFComponentsView()
+        }
+    }
+}
---
+++ DFComponents/Presentation/Scenes/DFComponentsView.swift
@@ -0,0 +1,21 @@
+//
+//  DFComponentsView.swift
+//  DFComponents
+//
+//  Created by ahmed maher on 29/01/2025.
+//
+
+import SwiftUI
+
+struct DFComponentsView: View {
+    var body: some View {
+        List {
+            Text("DFComponentsView")
+        }
+
+    }
+}
+
+#Preview {
+    DFComponentsView()
+}
