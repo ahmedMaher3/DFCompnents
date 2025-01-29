e2146f89edae8b3d7dec2a07e7c69be9b95467c9
EslamAbotaleb <eslamabotaleb07@gmail.com>
2025-01-29 18:11:03 +0200

    remove old implemenation for RadioButonViewModel

--- DFComponents/Presentation/UIComponents/RadioButton/ViewModel/RadioButtonViewModel.swift
+++ DFComponents/Presentation/UIComponents/RadioButton/ViewModel/RadioButtonViewModel.swift
@@ -7,45 +7,6 @@
 
 import Foundation
 
-/*
-final class RadioButtonViewModel: ObservableObject {
-
-    @Published var selectedItems: [String: RadioButtonModel] = [:]
-    @Published var questions: [String] = [
-        "What's your fav color ahmed?",
-        "What's your fav hassan?",
-        "What's your fav nasr?"
-    ]
-    @Published var radioButtonModels: [String: [RadioButtonModel]] = [
-        "question0": RadioButtonModel.elementsRadioButton,
-        "question1": RadioButtonModel.elementsRadioButton,
-        "question2": RadioButtonModel.elementsRadioButton
-    ]
-
-    var callbackAction: (RadioButtonModel?) -> Void
-
-    init(callbackAction: @escaping (RadioButtonModel?) -> Void = { _ in }) {
-        self.callbackAction = callbackAction
-    }
-
-    // Action to toggle the selection state for each question
-    func onTapRadioButton(questionID: String, item: RadioButtonModel) {
-        if selectedItems[questionID]?.id == item.id {
-            // Deselect if the same item is clicked again
-            selectedItems[questionID] = nil
-            callbackAction(nil)
-        } else {
-            // Select the new item
-            selectedItems[questionID] = item
-            callbackAction(item)
-        }
-    }
-    // Get the selected item for a specific question
-    func selectedItem(for questionID: String) -> RadioButtonModel? {
-        return selectedItems[questionID]
-    }
-}
-*/
 final class RadioButtonViewModel: ObservableObject {
 
     @Published var selectedItems: [String: String] = [:]  // Key: Question ID, Value: Selected item ID
