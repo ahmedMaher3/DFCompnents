6ef7d7107b44a8fd25973ec949a434c791b0a70f
EslamAbotaleb <eslamabotaleb07@gmail.com>
2025-02-12 17:37:22 +0200

    prepare page to display submit content

---
+++ DFComponents/Presentation/UIComponents/DataFlow/View/DisplayDataFormView.swift
@@ -0,0 +1,28 @@
+//
+//  DisplayDataFormView.swift
+//  DFComponents
+//
+//  Created by Eslam on 12/02/2025.
+//
+
+import SwiftUI
+
+struct DisplayDataFormView: View {
+    @ObservedObject var viewModel: DataFlowViewModel
+    var body: some View {
+            VStack(alignment: .leading, spacing: 16) {
+                Text("First Name: \(viewModel.firstName)")
+                Text("Last Name: \(viewModel.lastName)")
+                Text("Full Name: \(viewModel.fullName)")
+                Text("Email: \(viewModel.email)")
+                Text("Confirm Email: \(viewModel.confirmEmail)")
+            }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .padding(.horizontal, 16)
+        .padding(.vertical, 16)
+    }
+}
+
+#Preview {
+    DisplayDataFormView(viewModel: DataFlowViewModel())
+}
