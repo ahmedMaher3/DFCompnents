66ed96d8ad2cfc16036e43c550998768beffe5d1
EslamAbotaleb <eslamabotaleb07@gmail.com>
2025-03-05 16:30:16 +0200

    update function rendering fields

--- DFComponents/Presentation/Views/FormView/View/FormView.swift
+++ DFComponents/Presentation/Views/FormView/View/FormView.swift
@@ -91,7 +91,7 @@ struct fieldsListView: View {
     @ViewBuilder
     private func renderField(for field: FieldEntity) -> some View {
         switch field {
-            case .radio ((_, let radioViewModel)):
+            case .radio((_, let radioViewModel)):
                 ControlFormBuilderView {
                     EmptyView()
                 } controlType: {
@@ -100,17 +100,7 @@ struct fieldsListView: View {
                     EmptyView()
                 }
                 .opacity(radioViewModel.control.hidden ? 0 : 1)
-                .onReceive(
-                    radioViewModel.$control
-                        .map { $0.options }
-                        .debounce(for: .milliseconds(100), scheduler: DispatchQueue.main)
-                        .dropFirst()
-                        .removeDuplicates()
-                ) { newOptions in
-                    print("Updated options: \(newOptions)")
-                    // Call update in ViewModel to apply rule
-                    viewModel.applyFieldRules(by: field.id)
-                }
+
             case .textBox((_, let textBoxViewModel)):
                 ControlFormBuilderView {
                     EmptyView()
@@ -120,50 +110,155 @@ struct fieldsListView: View {
                     EmptyView()
                 }
                 .opacity(textBoxViewModel.control.hidden ? 0 : 1)
+
             case .number((_, let numberViewModel)):
                 ControlFormBuilderView {
-                    ///HeaderView
-                    HeaderComponentView(viewModel: HeaderComponentViewModel(baseProperties: numberViewModel.baseProperties!)) {
-                        HStack(alignment: .firstTextBaseline, spacing: 4) {
-                            if let label = numberViewModel.baseProperties?.label {
-                                Text(label)
-                                    .font(.headline)
-                                    .foregroundColor(.primary)
-                            }
-                            if let subLabel = numberViewModel.baseProperties?.subLabel {
-                                Text(subLabel)
-                                    .font(.subheadline)
-                                    .foregroundStyle(.red)
-                            }
-                            if let tooltip = numberViewModel.baseProperties?.tooltip {
-                                HStack {
-                                    Text("ⓘ")
-                                        .font(.system(size: 18))
-                                        .foregroundStyle(.gray)
-
-                                    Text(tooltip)
-                                        .font(.system(size: 20))
-                                        .foregroundStyle(.gray)
-                                        .offset(y: 5)
-                                }
-                            }
-                        }
-                    }
-                    .padding(.bottom, 8)
+                    renderHeader(for: numberViewModel.baseProperties)
                 } controlType: {
-                    ///Control
                     NumberFieldComponent(viewModel: numberViewModel)
                 } footerView: {
-                    ///FooterView
-                    if let interactiveProperties = numberViewModel.interactiveProperties {
-                        FooterComponentView(viewModel: FooterComponentViewModel(interactiveBaseProperties: numberViewModel.interactiveProperties!.base)) {
-                            HStack {
-                                Text(interactiveProperties.addNote == true ? "Note": "")
-                                Text(interactiveProperties.addAttachment == true ? "|| Attachment": "")
+                    renderFooter(for: numberViewModel.numberFieldModel.base)
+            }
+        }
+    }
+    /*
+     @ViewBuilder
+     private func renderField(for field: FieldEntity) -> some View {
+     switch field {
+     case .radio((_, let radioViewModel)):
+     ControlFormBuilderView {
+     EmptyView()
+     } controlType: {
+     RadioButtonView(radioButtonVM: radioViewModel)
+     } footerView: {
+     EmptyView()
+     }
+     .opacity(radioViewModel.control.hidden ? 0 : 1)
+     .onReceive(
+     radioViewModel.$control
+     .map { $0.options }
+     .debounce(for: .milliseconds(100), scheduler: DispatchQueue.main)
+     .dropFirst()
+     .removeDuplicates()
+     ) { newOptions in
+     print("Updated options: \(newOptions)")
+     viewModel.applyFieldRules(by: field.id)
+     }
+
+     case .textBox((_, let textBoxViewModel)):
+     ControlFormBuilderView {
+     EmptyView()
+     } controlType: {
+     TextBoxComponent(viewModel: textBoxViewModel)
+     } footerView: {
+     EmptyView()
+     }
+     .opacity(textBoxViewModel.control.hidden ? 0 : 1)
+
+     case .number((_, let numberViewModel)):
+     ControlFormBuilderView {
+     /// Header View
+     if let baseProperties = numberViewModel.baseProperties {
+     HeaderComponentView(viewModel: HeaderComponentViewModel(baseProperties: baseProperties)) {
+     HStack(alignment: .firstTextBaseline, spacing: 4) {
+     if let label = baseProperties.label {
+     Text(label)
+     .font(.headline)
+     .foregroundColor(.primary)
+     }
+     if let subLabel = baseProperties.subLabel {
+     Text(subLabel)
+     .font(.subheadline)
+     .foregroundStyle(.red)
+     }
+     if let tooltip = baseProperties.tooltip {
+     HStack {
+     Text("ⓘ")
+     .font(.system(size: 18))
+     .foregroundStyle(.gray)
+
+     Text(tooltip)
+     .font(.system(size: 20))
+     .foregroundStyle(.gray)
+     .offset(y: 5)
+     }
+     }
+     }
+     }
+     .padding(.bottom, 8)
+     }
+     } controlType: {
+     /// Control View
+     NumberFieldComponent(viewModel: numberViewModel)
+     } footerView: {
+     /// Footer View
+     if let interactiveProperties = numberViewModel.interactiveProperties {
+     FooterComponentView(viewModel: FooterComponentViewModel(interactiveBaseProperties: interactiveProperties.base)) {
+     HStack {
+     if interactiveProperties.addNote {
+     Text("Note")
+     }
+     if interactiveProperties.addAttachment {
+     Text("|| Attachment")
+     }
+     }
+     }
+     }
+     }
+     }
+     }
+     */
+    @ViewBuilder
+    private func renderHeader(for baseProperties: BaseProperties?) -> some View {
+        if let baseProperties = baseProperties {
+            HeaderComponentView(viewModel: HeaderComponentViewModel(baseProperties: baseProperties)) {
+                HStack(alignment: .firstTextBaseline, spacing: 4) {
+                    if let label = baseProperties.label {
+                        Text(label)
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                    }
+                    if let subLabel = baseProperties.subLabel {
+                        Text(subLabel)
+                            .font(.subheadline)
+                            .foregroundStyle(.red)
+                    }
+                    if let tooltip = baseProperties.tooltip {
+                        HStack {
+                            Text("ⓘ")
+                                .font(.system(size: 18))
+                                .foregroundStyle(.gray)
+
+                            Text(tooltip)
+                                .font(.system(size: 20))
+                                .foregroundStyle(.gray)
+                                .offset(y: 5)
                         }
                     }
                 }
             }
+            .padding(.bottom, 8)
+        } else {
+            EmptyView() // If no header is available
+        }
+    }
+
+    @ViewBuilder
+    private func renderFooter(for interactiveProperties: InteractiveField?) -> some View {
+        if let interactiveProperties = interactiveProperties {
+            FooterComponentView(viewModel:
+                                    FooterComponentViewModel(interactiveBaseProperties: interactiveProperties)) {
+                HStack {
+                    if interactiveProperties.addNote {
+                        Text("Note")
+                    }
+                    if interactiveProperties.addAttachment {
+                        Text("|| Attachment")
+                    }
+                }
+            }
+        } else {
+            EmptyView() // If no footer is available
         }
     }
 }
