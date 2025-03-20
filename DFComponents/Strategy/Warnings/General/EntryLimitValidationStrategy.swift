4a129c78ab1b3e2ebf80f107d5f8a936b4ba5ab5
EslamAbotaleb <eslamabotaleb07@gmail.com>
2025-03-20 12:28:19 +0200

    Update condition EntryLimitValidationStrategy

--- DFComponents/Strategy/Warnings/General/EntryLimitValidationStrategy.swift
+++ DFComponents/Strategy/Warnings/General/EntryLimitValidationStrategy.swift
@@ -7,10 +7,10 @@
 
 
 final class EntryLimitValidationStrategy: GeneralValidationStrategyProtocol {
-    private let minimumDigits: Int
-    private let maximumDigits: Int
+    private let minimumDigits: Int?
+    private let maximumDigits: Int?
 
-    init(minimumDigits: Int, maximumDigits: Int) {
+    init(minimumDigits: Int?, maximumDigits: Int?) {
         self.minimumDigits = minimumDigits
         self.maximumDigits = maximumDigits
     }
@@ -18,14 +18,15 @@ final class EntryLimitValidationStrategy: GeneralValidationStrategyProtocol {
     func validate(
         value: String?,
         fieldWarnings: inout [String]) {
-        guard let valueField = value else { return print("") }
-        let inputValue = valueField.replacingOccurrences(of: ".", with: "")
-
-        if inputValue.count < minimumDigits {
-            fieldWarnings.append("Minimum digits required: \(minimumDigits)")
+            guard let valueField = value else { return print("") }
+            let inputValue = valueField.replacingOccurrences(of: ".", with: "")
+            if let minimumDigits = minimumDigits,
+               inputValue.count < minimumDigits && minimumDigits != 0 {
+                fieldWarnings.append("Minimum digits required: \(minimumDigits)")
+            }
+            if let maximumDigits = maximumDigits,
+               inputValue.count > maximumDigits && maximumDigits != 0 {
+                fieldWarnings.append("Maximum digits allowed: \(maximumDigits)")
+            }
         }
-        if inputValue.count > maximumDigits {
-            fieldWarnings.append("Maximum digits allowed: \(maximumDigits)")
-        }
-    }
 }
