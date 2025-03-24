f927b286fbd88bdf8b6cd1aec1af1aa67bccfd37
EslamAbotaleb <eslamabotaleb07@gmail.com>
2025-03-24 12:25:13 +0200

    Organize Classic Page Header View

--- DFComponents/Presentation/UIComponents/PageHeader/ClassicPageHeaderView.swift
+++ DFComponents/Presentation/UIComponents/PageHeader/ClassicPageHeaderView.swift
@@ -12,61 +12,67 @@ struct ClassicPageHeaderView: View {
 
     var body: some View {
         VStack {
-            VStack(alignment: .leading, spacing: viewModel.headerData.isExpanded ? 0 : 8) {
-                if viewModel.headerData.isExpanded {
-                    Text(viewModel.headerData.title)
-                        .font(.system(size: 18, weight: .bold))
-                        .foregroundColor(Color(hex: "#173E67"))
-                        .lineLimit(2)
-                        .padding(.bottom, 6)
-
-                    Text(viewModel.headerData.description)
-                        .font(.system(size: 14))
-                        .foregroundColor(Color(hex: "#173E67"))
-                        .lineLimit(5)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .onTapGesture {
-                            withAnimation {
-                                self.viewModel.toggleExpanded()
-                            }
-                        }
-
-                } else {
-                    HStack(spacing: 8) {
-                        if let iconURL = URL(string: self.viewModel.headerData.logo) {
-                            AsyncImage(url: iconURL) { image in
-                                image.resizable()
-                                    .resizable()
-                                    .frame(width: 20, height: 20)
-                                    .foregroundColor(Color(hex: "#173E67"))
-                            } placeholder: {
-                                ProgressView()
-                            }
-                        } else {
-                            Image("info.circle.fill")
-                                .foregroundColor(Color(hex: "#173E67"))
-                        }
-
-                        Text(viewModel.headerData.title)
-                            .font(.system(size: 16, weight: .bold))
-                            .foregroundColor(Color(hex: "#173E67"))
-                            .lineLimit(1)
-                    }
-                    .onTapGesture {
-                        withAnimation {
-                            self.viewModel.toggleExpanded()
-                        }
+            contentView
+                .onTapGesture {
+                    withAnimation {
+                        viewModel.toggleExpanded()
                     }
                 }
-            }
-            .padding([.top, .leading, .trailing])
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .background(Color(hex: "#FAFBFF"))
+                .padding([.top, .leading, .trailing])
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color(hex: "#FAFBFF"))
 
             Divider()
                 .frame(height: 1)
                 .background(Color.gray)
-                .padding(.bottom, 4)
+                .padding([.top,.bottom], 4)
+        }
+    }
+
+    @ViewBuilder
+    private var contentView: some View {
+        if viewModel.headerData.isExpanded {
+            expandedView
+        } else {
+            collapsedView
+        }
+    }
+
+    /// Expanded View
+    private var expandedView: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text(viewModel.headerData.title)
+                .font(.system(size: 18, weight: .bold))
+                .foregroundColor(.primaryBlue)
+                .lineLimit(2)
+
+            Text(viewModel.headerData.description)
+                .font(.system(size: 14))
+                .foregroundColor(.primaryBlue)
+                .lineLimit(5)
+                .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+
+    /// Collapsed View
+    private var collapsedView: some View {
+        HStack(spacing: 8) {
+            if let iconURL = URL(string: viewModel.headerData.logo) {
+                AsyncImage(url: iconURL) { image in
+                    image.resizable()
+                        .frame(width: 20, height: 20)
+                } placeholder: {
+                    ProgressView()
+                }
+            } else {
+                Image(systemName: "info.circle.fill")
+                    .foregroundColor(.primaryBlue)
+            }
+
+            Text(viewModel.headerData.title)
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(.primaryBlue)
+                .lineLimit(1)
         }
     }
 }
